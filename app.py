0b3a9c2c5c450cddab630de9e20e8f43ff9b3bcb
Avinash Patel <168765546+AvinashWorks-07@users.noreply.github.com>
2025-12-29 21:58:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="Fake News Detection")
 
 st.title("Fake News Detection App 🚀")
-
-st.write("If you can see this, your app is running successfully!")
+st.write("App is running successfully!")
 
 st.success("Deployment Successful 🎉")
